a7090cc81b5eae348bb37661e16cf56c0084ca22
ufuksonmezua <ufukasonmez@gmail.com>
2023-08-06 15:52:28 +0300

    1550-060823

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-console.log("Merhaba Javascript");
+// ################################################################
+            //////////////// 05-08-2023 ////////////////
+// ################################################################
+
+// console.log("Merhaba Javascript");
 
 //console.log("Bu bir yorum satırıdır.");
 
@@ -9,76 +13,130 @@ console.log("Merhaba Javascript");
 
 // değişkenler ve değişken oluşturma
 
-const isim = "Ufuk"; //string değişken türü, düz yazı tipindeki değerleri tutar ve bunu tırnak işaretleri içerisin de tutar.
-let soyisim;
+// const isim = "Ufuk"; //string değişken türü, düz yazı tipindeki değerleri tutar ve bunu tırnak işaretleri içerisin de tutar.
+// let soyisim;
 
 // constisim; bu şekilde 
 
-soyisim = "Sönmez";
+// soyisim = "Sönmez";
 // isim = "Yeni İsim";  const ön ekiyle oluşturulan değişkenlere tekrar yeni değer tanımlaması yapılamaz!!
 
-const tamisim = isim + soyisim
+// const tamisim = isim + soyisim
 
 // soyisim = "Yeni Soyad";  --> buradaki soyisim değeri console'da güncellenmeyecektir.
 
-console.log(tamisim);
+// console.log(tamisim);
 
 
-const number1 = 5;  // number
-const number2 = 10; // number
-const number3 = "4"; // string
+// const number1 = 5;  // number
+// const number2 = 10; // number
+// const number3 = "4"; // string
 
 
-const calculate1 = number2 * number1; //çarpma işlemi (sonuç: 50)
-const calculate2 = number2 / number1; //bölme işlemi (sonuç: 2) 
-const calculate3 = number2 + number1; //toplama işlemi (sonuç: 15)
-const calculate4 = number2 - number1; //çıkarma işlemi (sonuç: 5)
-const calculate5 = 30 * 2;
+// const calculate1 = number2 * number1; //çarpma işlemi (sonuç: 50)
+// const calculate2 = number2 / number1; //bölme işlemi (sonuç: 2) 
+// const calculate3 = number2 + number1; //toplama işlemi (sonuç: 15)
+// const calculate4 = number2 - number1; //çıkarma işlemi (sonuç: 5)
+// const calculate5 = 30 * 2;
 
-const calculate6_string = number2 + number3; //
-const calculate6 = number2 + Number(number3);
+// const calculate6_string = number2 + number3; //
+// const calculate6 = number2 + Number(number3);
 
-const stringNumber = number1.toString();
-const calculateStringNumber = stringNumber + number2;
+// const stringNumber = number1.toString();
+// const calculateStringNumber = stringNumber + number2;
 
-console.log(calculate1);
-console.log(calculate2);
-console.log(calculate3);
-console.log(calculate4);
-console.log(calculate5);
-console.log(calculate6_string);
-console.log(calculate6);
-console.log(calculateStringNumber);
+// console.log(calculate1);
+// console.log(calculate2);
+// console.log(calculate3);
+// console.log(calculate4);
+// console.log(calculate5);
+// console.log(calculate6_string);
+// console.log(calculate6);
+// console.log(calculateStringNumber);
 
-const newVaraible =  number2 + "-" + number3;
-console.log(newVaraible);
+// const newVaraible =  number2 + "-" + number3;
+// console.log(newVaraible);
 
 // ################################################################
     //////////////// DEĞİŞKEN OLUŞTURMA DEVAM ////////////////
 // ################################################################
 
 
-const sayi1 = 5;
-const sayi2 = 10;
-const sayi3 = 15;
-const sayi4 = 20;
-const sayi5 = 25;
+// const sayi1 = 5;
+// const sayi2 = 10;
+// const sayi3 = 15;
+// const sayi4 = 20;
+// const sayi5 = 25;
 
-const sayilar = [5, 10, 15, 20, 25, "selam", "deneme", 3, 5, 10, 33]; // array (liste) tipi değişken
-const ilksayi = sayilar[0];
+// const sayilar = [5, 10, 15, 20, 25, "selam", "deneme", 3, 5, 10, 33]; // array (liste) tipi değişken
+// const ilksayi = sayilar[0];
 
-console.log(sayilar);
-console.log("sayılar array'in elaman sayısı:", sayilar.length);
-console.log("array'in ilk elamanı:", ilksayi);
-console.log("array'in ikinci elamanı:", sayilar[1]);
-console.log("array'in son elamanı:", sayilar[5]);
+// console.log(sayilar);
+// console.log("sayılar array'in elaman sayısı:", sayilar.length);
+// console.log("array'in ilk elamanı:", ilksayi);
+// console.log("array'in ikinci elamanı:", sayilar[1]);
+// console.log("array'in son elamanı:", sayilar[5]);
 
-const elamansayisi = sayilar.length; // 11
-const soneleman = sayilar[elamansayisi - 1];
-console.log("son eleman:", soneleman);
+// const elamansayisi = sayilar.length; // 11
+// const soneleman = sayilar[elamansayisi - 1];
+// console.log("son eleman:", soneleman);
 
 
-const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-const toplamsayi = numbers.length;
-const calc = numbers[0] * numbers[toplamsayi - 1];
-console.log("ilk ve son eleman çarpım sonucu:", calc);
+// const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// const toplamsayi = numbers.length;
+// const calc = numbers[0] * numbers[toplamsayi - 1];
+// console.log("ilk ve son eleman çarpım sonucu:", calc);
+
+// ################################################################
+            //////////////// 06-08-2023 ////////////////
+// ################################################################
+
+// const x = 50.124;
+// const y = x.toFixed(3); // tofixed() methodu ile ondalıklı sayıların virgülden sonraki kaç hane gösterileceğini belirler.
+// console.log("", x);
+
+
+// const x_degisken = "28.55";
+
+// const string_number_cevirme = Number(x_degisken);
+// const string_number_cevirme_fixed = string_number_cevirme.toFixed(2);
+// console.log("Deneme", string_number_cevirme_fixed);
+
+// const veri_tipi_ogrenme_x = typeof (x_degisken);
+// console.log("x değişkeninin veri tipi:", veri_tipi_ogrenme_x);
+
+// let ogrenciler = [
+//     "Canberk",  //0.index
+//     "Ahmet",    //1.index
+//     "Mehmet",   //2.index
+//     "Veli",     //3.index
+//     5,          //4.index (array tipi değişkenler de karışık veri tipleri tutulabilir)
+//     "Ayşe",     //5.index
+//     "Fatma",    //6.index
+//     "Hasan"     //7.index  
+//  ];  // array(liste) veri tipi
+
+
+//     const ilk_ogrenci = ogrenciler[0];
+//     const ogrencisayisi = ogrenciler.length;
+//     console.log("Öğrenciler Array'i", ogrenciler);
+//     console.log("Öğrenci Sayısı:", ogrencisayisi);
+//     console.log("İlk Öğrenci:", ilk_ogrenci);
+
+
+//     const sonogrenci = ogrenciler[ogrencisayisi - 1];
+//     console.log("Son Öğrenci:", sonogrenci);
+
+//     ogrenciler.push("Sevgi");
+
+//     console.log("Öğrenciler Array'i Güncellendi", ogrenciler);
+
+let meyveler = ["Kavun","Karpuz","Üzüm","Kiraz","Şeftali"];
+meyveler.push = ("Muz");
+const ilkmeyve = meyveler[0]
+console.log(meyveler);
+console.log("ilk Meyve:", ilkmeyve);
+const meyvesayisi = meyveler.length;
+const sonmeyve = meyveler[meyvesayisi - 1];
+console.log("Son Meyve:", sonmeyve);
+
